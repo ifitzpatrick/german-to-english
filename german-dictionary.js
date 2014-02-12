33a6bb7abf73ad891fb224b7e287176322e77809
Ian Fitzpatrick <ian@grapheffect.com>
2014-02-11 17:15:28 -0800

    german dictionary script

---
+++ german-dictionary.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name       german-dictionary
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      http://tampermonkey.net/index.php?version=3.6.3737.80&ext=dhdg&updated=true
+// @copyright  2012+, You
+// @include    http://www.dw.de/*
+// @include    http://www.spiegel.de
+// @include    http://www.spiegel.de/*
+// @include    http://www.sueddeutsche.de
+// @include    http://www.sueddeutsche.de/*
+// ==/UserScript==
+
+var $ = function (selector) {
+      return document.querySelectorAll(selector);
+    },
+
+    currentPopover = null,
+    removePopover = function () {
+      if (currentPopover) {
+        currentPopover.remove();
+       currentPopover = null;
+      }
+    },
+    popover = function (x, y, search) {
+      var div    = document.createElement("div"),
+          body   = $("body")[0],
+          iframe = document.createElement("iframe"),
+          width  = 600,
+          height = 800,
+          url    = "http://dict.leo.org/#/search=" + search + "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on";
+      
+      iframe.setAttribute("src", url);
+      iframe.width = width;
+      iframe.height = height;
+      div.style.width = width + "px";
+      div.style.height = height + "px";
+      div.style.position = "absolute";
+      div.style.left = x.toString() + "px";
+      div.style.top = y.toString() + "px";
+      body.appendChild(div);
+      div.appendChild(iframe);
+      
+      removePopover();
+      currentPopover = div;
+    };
+
+document.addEventListener("keydown", function (event) {
+  var selection = document.getSelection(), node, text, x, y;
+  if (event.which === 68 && event.ctrlKey && selection) {
+    node = selection.anchorNode.parentElement;
+    text = selection.toString();
+    x = node.offsetLeft + node.offsetWidth;
+    y = node.offsetTop;
+    popover(x, y, text);
+  } else if (event.which === 27) {
+    removePopover();
+  }
+});
+

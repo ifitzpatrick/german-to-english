e39c09147a779528955525bd97fa348de60d0548
Ian Fitzpatrick <ian@grapheffect.com>
2014-02-12 13:58:18 -0800

    Use xmlhttprequest and domparser to extract info

--- dest/index.js
+++ dest/index.js
@@ -14,7 +14,8 @@
 
 
 (function() {
-  var $, currentPopover, popover, removePopover;
+  var $, createElement, currentPopover, dataTree, nodeToJSON, popover, replacePopover, request,
+    __hasProp = {}.hasOwnProperty;
 
   $ = function(selector) {
     return document.querySelectorAll(selector);
@@ -22,33 +23,139 @@
 
   currentPopover = null;
 
-  removePopover = function() {
+  replacePopover = function(newPopover) {
+    var body;
+    if (newPopover == null) {
+      newPopover = null;
+    }
+    body = $("body")[0];
     if (currentPopover) {
       currentPopover.remove();
     }
-    return currentPopover = null;
+    if (newPopover) {
+      body.appendChild(newPopover);
+    }
+    return currentPopover = newPopover;
+  };
+
+  createElement = function(tag, styles) {
+    var ele, key, value;
+    ele = document.createElement(tag);
+    for (key in styles) {
+      if (!__hasProp.call(styles, key)) continue;
+      value = styles[key];
+      ele.style[key] = value;
+    }
+    return ele;
+  };
+
+  request = function(url) {
+    return new Promise(function(resolve, reject) {
+      return GM_xmlhttpRequest({
+        method: "GET",
+        url: url,
+        onload: function(res) {
+          return resolve(res);
+        },
+        onerror: function(error) {
+          return reject(error);
+        }
+      });
+    });
+  };
+
+  nodeToJSON = function(dom, nodeSpec, tree) {
+    var attrName, childNodeSpec, element, key, node, nodes;
+    if (tree == null) {
+      tree = {};
+    }
+    tree[nodeSpec.name] = nodes = (function() {
+      var _i, _j, _len, _len1, _ref, _ref1, _ref2, _results;
+      _ref = dom.querySelectorAll(nodeSpec.selector);
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        element = _ref[_i];
+        node = {};
+        _ref1 = nodeSpec.attrs || {};
+        for (key in _ref1) {
+          if (!__hasProp.call(_ref1, key)) continue;
+          attrName = _ref1[key];
+          node[key] = element.getAttribute(attrName);
+        }
+        if (nodeSpec.text) {
+          node["text"] = element.textContent;
+        }
+        _ref2 = nodeSpec.children || [];
+        for (_j = 0, _len1 = _ref2.length; _j < _len1; _j++) {
+          childNodeSpec = _ref2[_j];
+          nodeToJSON(element, childNodeSpec, node);
+        }
+        _results.push(node);
+      }
+      return _results;
+    })();
+    return tree;
+  };
+
+  dataTree = function(xml, spec) {
+    var dom, parser;
+    parser = new DOMParser;
+    dom = parser.parseFromString(xml, "application/xml");
+    return nodeToJSON(dom, spec);
   };
 
   popover = function(x, y, search) {
-    var body, div, height, iframe, url, width;
-    div = document.createElement("div");
-    body = $("body")[0];
-    iframe = document.createElement("iframe");
+    var div, height, url, width;
     width = 600;
     height = 800;
-    url = "http://dict.leo.org/#/search=" + search + "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on";
-    iframe.setAttribute("src", url);
-    iframe.width = width;
-    iframe.height = height;
-    div.style.width = width + "px";
-    div.style.height = height + "px";
-    div.style.position = "absolute";
-    div.style.left = x.toString() + "px";
-    div.style.top = y.toString() + "px";
-    body.appendChild(div);
-    div.appendChild(iframe);
-    removePopover();
-    return currentPopover = div;
+    div = createElement("div", {
+      width: width + "px",
+      height: height + "px",
+      position: "absolute",
+      left: x.toString() + "px",
+      top: y.toString() + "px"
+    });
+    url = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?search=" + search;
+    div.innerText = "LOADING...";
+    replacePopover(div);
+    return request(url).then(function(res) {
+      var body, e, spec, text, tree;
+      try {
+        body = res.responseText;
+        spec = {
+          name: "sections",
+          selector: "section",
+          attrs: {
+            sectionName: "sctTitle"
+          },
+          children: [
+            {
+              name: "definitions",
+              selector: "entry",
+              children: [
+                {
+                  name: "langs",
+                  selector: "side",
+                  attrs: {
+                    lang: "lang"
+                  },
+                  text: true
+                }
+              ]
+            }
+          ]
+        };
+        tree = dataTree(body, spec);
+        text = JSON.stringify(tree, null, 4);
+        div.innerHtml = "";
+        return div.innerText = text;
+      } catch (_error) {
+        e = _error;
+        debugger;
+      }
+    }, function() {
+      return div.innerText = "ERROR";
+    });
   };
 
   document.addEventListener("keydown", function(event) {
@@ -61,7 +168,7 @@
       y = node.offsetTop;
       return popover(x, y, text);
     } else if (event.which === 27) {
-      return removePopover();
+      return replacePopover();
     }
   });
 
--- src/main.coffee
+++ src/main.coffee
@@ -3,33 +3,102 @@ $ = (selector) ->
 
 currentPopover = null
 
-removePopover = ->
+replacePopover = (newPopover = null) ->
+  body = $("body")[0]
   if currentPopover
     currentPopover.remove()
+  
+  if newPopover
+    body.appendChild newPopover
 
-  currentPopover = null
+  currentPopover = newPopover
+
+createElement = (tag, styles) ->
+  ele = document.createElement tag
+  for own key, value of styles
+    ele.style[key] = value
+
+  return ele
+
+request = (url) ->
+  new Promise (resolve, reject) ->
+    GM_xmlhttpRequest
+      method: "GET"
+      url: url
+      onload: (res) ->
+        resolve res
+
+      onerror: (error) ->
+        reject error
+
+nodeToJSON = (dom, nodeSpec, tree = {}) ->
+  tree[nodeSpec.name] = nodes =
+    for element in dom.querySelectorAll nodeSpec.selector
+      node = {}
+      for own key, attrName of nodeSpec.attrs or {}
+        node[key] = element.getAttribute attrName
+
+      if nodeSpec.text
+        node["text"] = element.textContent
+
+      for childNodeSpec in nodeSpec.children or []
+        nodeToJSON element, childNodeSpec, node
+
+      node
+
+  return tree
+
+dataTree = (xml, spec) ->
+  parser = new DOMParser
+  dom = parser.parseFromString xml, "application/xml"
+  nodeToJSON dom, spec
 
 popover = (x, y, search) ->
-  div    = document.createElement("div")
-  body   = $("body")[0]
-  iframe = document.createElement("iframe")
   width  = 600
   height = 800
-  url    = "http://dict.leo.org/#/search=#{search}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on"
+  div    = createElement "div",
+    width:    width + "px"
+    height:   height + "px"
+    position: "absolute"
+    left:     x.toString() + "px"
+    top:      y.toString() + "px"
 
-  iframe.setAttribute "src", url
-  iframe.width = width
-  iframe.height = height
-  div.style.width = width + "px"
-  div.style.height = height + "px"
-  div.style.position = "absolute"
-  div.style.left = x.toString() + "px"
-  div.style.top = y.toString() + "px"
-  body.appendChild div
-  div.appendChild iframe
+  url  =
+    "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?search=#{search}"
 
-  removePopover()
-  currentPopover = div
+  div.innerText = "LOADING..."
+  replacePopover(div)
+  request(url).then (res) ->
+    try
+      body = res.responseText
+      spec =
+        name: "sections"
+        selector: "section"
+        attrs:
+          sectionName: "sctTitle"
+
+        children: [
+          name: "definitions"
+          selector: "entry"
+          children: [
+            name: "langs"
+            selector: "side"
+            attrs:
+              lang: "lang"
+
+            text: true
+          ]
+        ]
+      tree = dataTree body, spec
+      text = JSON.stringify tree, null, 4
+      div.innerHtml = ""
+      div.innerText = text
+
+    catch e
+      debugger
+
+  , ->
+    div.innerText = "ERROR"
 
 document.addEventListener "keydown", (event) ->
   selection = document.getSelection()
@@ -41,5 +110,5 @@ document.addEventListener "keydown", (event) ->
     popover x, y, text
 
   else if event.which is 27
-    removePopover()
+    replacePopover()
 
